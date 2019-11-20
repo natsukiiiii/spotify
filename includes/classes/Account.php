b1b9934d70fc601a6a2f34973e559882ca3d13be
watanabe <watanabe@ilovex.co.jp>
2019-11-20 15:12:13 +0900

    validate the username

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,21 +1,29 @@
 <?php
 class Account{
+    private $errorArray;
+    
     public function __construct() {
-
+      $this->errorArray = array();
     }
 
-    public function register(){
-        $this->validataUsername($username);
-        $this->validataFirstName($firstName);
-        $this->validataLastName($lastName);
-        $this->validataEmails($email,$email2);        
-        $this->validataPasswords($password,$password2);
+    public function register($un,$fn,$ln,$em,$em2,$pw,$pw2){
+        $this->validataUsername($un);
+        $this->validataFirstName($fn);
+        $this->validataLastName($ln);
+        $this->validataEmails($em,$em2);        
+        $this->validataPasswords($pw,$pw2);
     
     
     }
 
     private function validataUsername($un){
-        echo "username function called";
+      
+        if(strlen($un) > 25 || strlen($un) < 5){
+         array_push($this->errorArray,"your username must be between 5 and 25 characters");
+         return;
+        }
+
+        //todo: check is username exists
 
     }
     
