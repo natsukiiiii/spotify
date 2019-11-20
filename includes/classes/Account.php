2759ba5440722d1887cd9f1cbe6dd1613ba70ff6
watanabe <watanabe@ilovex.co.jp>
2019-11-20 15:53:26 +0900

    validating the passwords

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -28,12 +28,31 @@ class Account{
     }
     
     private function validataFirstName($fn){
-        
+        if(strlen($fn) > 25 || strlen($fn) < 2){
+            array_push($this->errorArray,"your first name must be between 5 and 25 characters");
+            return;
+           } 
     }
+
     private function validataLastName($ln){
-        
+        if(strlen($ln) > 25 || strlen($ln) < 2){
+            array_push($this->errorArray,"your last name must be between 5 and 25 characters");
+            return;
+           }  
     }
+
     private function validataEmails($em,$em2){
+        if($em != $em2) {
+            array_push($this->errorArray,"your emails don't much");
+            return;
+        }
+        if(!filter_var($em,FILTER_VALIDATE_EMAIL)){
+            //filter_varでメールが正しいか正しくないか(RFCに準拠しているという意味で)チェックする
+            array_push($this->errorArray,"email is invalid");
+            //メールが正しくない場合は、エラー文出す
+            return;
+        }
+        //todo: check that username hasn't already been used　ユーザー名がまだ使用されていないことを確認してください
         
     }
     private function validataPasswords($pw,$pw2){
